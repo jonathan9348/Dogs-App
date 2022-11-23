9783ead9fffdf9e25f0a149b4322ac1f00f0bf1b
unknown <jonathan.moran9348@gmail.com>
2022-11-23 15:27:55 -0300

    Terminadisisisisisisisisismo~

--- client/src/components/DogDetails.js
+++ client/src/components/DogDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { dogsDetails } from "../redux/actions";
 import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import "./style/DogDetails.css";
 
 
 export default function DogDetails() {
@@ -22,10 +23,10 @@ export default function DogDetails() {
       }
 
   return (
-    <div className="content">
+    <div>
       
-      <div>
-        <h1 className="title-page">
+      <div className="title-page">
+        <h1 >
           Here you will find all the data of your puppy
         </h1>
       </div>
@@ -39,29 +40,31 @@ export default function DogDetails() {
       <img src={detailDog.image} alt='Image not found' className='img-st'/>
 
       <div>
-        <h1 className="title-dog">
-          {detailDog.temperament || detailDog.temperaments?.map(e => e.name + " ")}
+        <h1 className="title-temp">
+          Temperaments: {detailDog.temperament || detailDog.temperaments?.map(e => e.name + " ")}
         </h1>
       </div>
 
       <div>
       
-      <h4>
+      <h4 className="descr">
         Height: {`${detailDog.heightMin} - ${detailDog.heightMax}cm`}
       </h4>
+      <hr/>
       
-      <h4>
+      <h4 className="descr">
         Weight: {`${detailDog.weightMin} - ${detailDog.weightMax}kg`}
       </h4>
+      <hr/>
       
-      <h4>
+      <h4 className="descr">
         Life Span: {" "}
          {detailDog.createInDb ? `${detailDog.lifeSpan} years` : `${detailDog.lifeSpan}`}
       </h4>
 
       </div>
 
-      <button onClick={handleClick}>BACK HOME</button>
+      <button onClick={handleClick} className="btnback">BACK HOME</button>
 
     </div>
   );
